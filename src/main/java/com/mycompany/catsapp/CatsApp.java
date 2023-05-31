e4ff6915e7f0e1de5531fa6482474e94c318ea2e
Carlos Herrera <carlos4505@gmail.com>
2023-05-31 03:00:03 -0500

    Carga del proyecto

---
+++ src/main/java/com/mycompany/catsapp/CatsApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.catsapp;
+
+/**
+ *
+ * @author soycarlosherrera
+ */
+public class CatsApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
